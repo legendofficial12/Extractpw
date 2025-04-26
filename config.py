92bde1e90c262b952cc55812e4a3bdc9406b8cea
legendofficial12 <skphotograph029@gmail.com>
2025-04-26 19:36:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "29500466"
+api_hash = "97de63084b44bb7ca5ab11d1252fa098"
+bot_token = "7995877850:AAFhmy6B65_Q_GBZXz1NKj7ioarOOENHm_U"
+auth_users = [7890781002,7009468802]
+sudo_users = [7890781002,7009468802]
